b3235d79fa9238cce3ac5cd98fc3393a3aa9e0f8
Guido <molido@hotmail.it>
2020-08-26 15:47:54 +0200

    Primo Commit v1

--- js/main.js
+++ js/main.js
@@ -1,37 +1,11 @@
 // GOAL: replicare quanto visto a lezione sulla todo-list permettendo all'utente di leggere tutti i task inseriti, inserirne di nuovi, eliminare quelli vecchi
 
-function searchFilm(ricercaUtente, apiKey) {
+function getList () {
   $.ajax({
-    url: "https://api.themoviedb.org/3/search/movie",
+    url: "http://157.230.17.132:3019/todos",
     method: "GET",
-    data: {
-      api_key: apiKey,
-      query: ricercaUtente
-    },
     success: function(data, state){
 
-      var resultsNum = data["total_results"];
-      var filmList = data["results"];
-
-
-      if (filmList) {
-
-        var template = $("#film-template").html();
-        var target = $("#filmList");
-        var compiled = Handlebars.compile(template);
-
-        for (var i = 0; i < filmList.length; i++) {
-          var film = filmList[i];
-
-          var vote = film["vote_average"];
-          var language = film["original_language"];
-          var poster = film["poster_path"];
-          film.stars = starredVote(vote);
-          film.flag = languageIcon(language);
-          film.copertina = copertinaUrl(poster);
-
-          var filmHTML = compiled(film);
-          target.append(filmHTML);
         }
 
       }
